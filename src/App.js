42bc80d67c49ef6236e258ea5acaf320717d3c9c
Unknown <r.chase.weller@gmail.com>
2017-04-05 10:48:53 -0600

    simple page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from 'material-ui/AppBar';
+
 
 class App extends Component {
   render() {
@@ -8,10 +10,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to my website</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <AppBar />
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from './App';
 import './index.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+
+injectTapEventPlugin();
+
 
 ReactDOM.render(
-  <App />,
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+  <App title="My Website" />
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
+
